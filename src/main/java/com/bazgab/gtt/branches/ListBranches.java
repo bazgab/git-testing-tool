73a3d2281d26b42de54fe9f1db6d6f9d7885db17
Gabriel Baz <gabrielbzzt@gmail.com>
2025-04-16 16:28:46 -0300

    <Refactor>(Branches): added better formatting for the ListBranches class

--- src/main/java/com/bazgab/gtt/branches/ListBranches.java
+++ src/main/java/com/bazgab/gtt/branches/ListBranches.java
@@ -8,6 +8,7 @@ import org.eclipse.jgit.lib.Repository;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 import static com.bazgab.gtt.repositories.FindRepository.openJGitRepository;
 
@@ -16,18 +17,34 @@ public class ListBranches {
     public static void main(String[] args) throws IOException, GitAPIException {
         try (Repository repository = openJGitRepository()) {
             try (Git git = new Git(repository)) {
+                System.out.println("=================================================================================================================");
+                System.out.println("BRANCH INFO");
+                System.out.println("=================================================================================================================");
                 System.out.println("Local branches:");
                 List<Ref> call = git.branchList().call();
                 for (Ref ref : call) {
-                    System.out.printf("    %s\n", ref.getName());
+                    String BranchNameInput = ref.getName();
+                    Scanner s = new Scanner(BranchNameInput).useDelimiter("\\s*/\\s*");
+                    // Adding this to skip the word "refs" on every branch, since it comes from the jgit.lib.refs api
+                    s.skip("refs");
+                    System.out.printf("    %s\n", s.next());
+                    System.out.printf("      %s\n", s.next());
+                    s.close();
 
                 }
-
+                System.out.println("-----------------------------------------------------------------------------------------------------------------");
                 System.out.println("Remote branches:");
                 call = git.branchList().setListMode(ListBranchCommand.ListMode.ALL).call();
                 for (Ref ref : call) {
-                    System.out.printf("    %s\n", ref.getName());
+                    String BranchNameInput = ref.getName();
+                    Scanner s = new Scanner(BranchNameInput).useDelimiter("\\s*/\\s*");
+                    s.skip("refs");
+                    System.out.printf("    %s\n", s.next());
+                    System.out.printf("      %s\n", s.next());
+                    s.close();
                 }
+                System.out.println("=================================================================================================================");
+
             }
         }
     }
