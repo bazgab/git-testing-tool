289e2b3aeea5033b7237436fdc7e2a66530fe3ca
Gabriel Baz <gabrielbzzt@gmail.com>
2025-04-16 14:26:26 -0300

    <Feat>(Repositories): added repositories package to isolate logic

---
+++ src/main/java/com/bazgab/repositories/FindRepository.java
@@ -0,0 +1,18 @@
+package com.bazgab.repositories;
+
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+
+import java.io.IOException;
+
+public class FindRepository {
+
+    public static Repository openJGitRepository() throws IOException {
+        FileRepositoryBuilder builder = new FileRepositoryBuilder();
+        return builder
+                .readEnvironment() // scan environment GIT_* variables
+                .findGitDir() // scan up the file system tree
+                .build();
+    }
+
+}
--- src/main/java/com/bazgab/status/ShowStatus.java
+++ src/main/java/com/bazgab/status/ShowStatus.java
@@ -5,21 +5,13 @@ import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.Status;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
 import java.io.IOException;
 
+import static com.bazgab.repositories.FindRepository.openJGitRepository;
+
 
 public class ShowStatus {
-
-    public static Repository openJGitRepository() throws IOException {
-        FileRepositoryBuilder builder = new FileRepositoryBuilder();
-        return builder
-                .readEnvironment() // scan environment GIT_* variables
-                .findGitDir() // scan up the file system tree
-                .build();
-    }
-
 
     public static void main(String[] args) throws GitAPIException {
 
