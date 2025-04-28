73daeabb4961e3ad8cbbf6e4830af9b12302213d
Gabriel Baz <gabrielbzzt@gmail.com>
2025-04-28 11:12:04 -0300

    <Feat>(Logging): added ConvertSecToDays function for ease of readability

--- src/main/java/com/bazgab/gtt/logs/ShowLog.java
+++ src/main/java/com/bazgab/gtt/logs/ShowLog.java
@@ -11,87 +11,51 @@ import static com.bazgab.gtt.repositories.FindRepository.openJGitRepository;
 
 public class ShowLog {
 
+    public static String ConvertSectoDay(int n) {
+        int day = n / (24 * 3600);
+
+        n = n % (24 * 3600);
+        int hour = n / 3600;
+
+        n %= 3600;
+        int minutes = n / 60 ;
+
+        n %= 60;
+        int seconds = n;
+
+        return STR."\{day} days \{hour} hours \{minutes} minutes \{seconds} seconds ";
+    }
+
     @SuppressWarnings("unused")
     public static void main(String[] args) throws IOException, GitAPIException {
+        
         try (Repository repository = openJGitRepository()) {
             try (Git git = new Git(repository)) {
                 Iterable<RevCommit> logs = git.log()
                         .call();
                 int count = 0;
                 for (RevCommit rev : logs) {
-                    //System.out.println("Commit: " + rev /* + ", name: " + rev.getName() + ", id: " + rev.getId().getName() */);
+                    System.out.println("Commit: " + rev
+                            + " Name: " + rev.getName()
+                            + " Id: " + rev.getId().getName()
+                            + " Time: " + ConvertSectoDay(rev.getCommitTime()));
                     count++;
                 }
                 System.out.println("Had " + count + " commits overall on current branch");
 
-                System.out.println("Listing commits on testbranch");
-                logs = git.log()
-                        .add(repository.resolve("remotes/origin/testbranch"))
-                        .call();
-                count = 0;
-                for (RevCommit rev : logs) {
-                    System.out.println("Commit: " + rev /* + ", name: " + rev.getName() + ", id: " + rev.getId().getName() */);
-                    count++;
-                }
-                System.out.println("Had " + count + " commits overall on test-branch");
 
-                System.out.println("Listing commits on testbranch but not on master (i.e. unmerged)");
-                logs = git.log()
-                        .not(repository.resolve("master"))
-                        .add(repository.resolve("remotes/origin/testbranch"))
-                        .call();
-                count = 0;
-                for (RevCommit rev : logs) {
-                    System.out.println("Commit: " + rev /* + ", name: " + rev.getName() + ", id: " + rev.getId().getName() */);
-                    count++;
-                }
-                System.out.println("Had " + count + " commits only on test-branch");
-
-                logs = git.log()
-                        .all()
-                        .call();
-                count = 0;
-                for (RevCommit rev : logs) {
-                    //System.out.println("Commit: " + rev /* + ", name: " + rev.getName() + ", id: " + rev.getId().getName() */);
-                    count++;
-                }
                 System.out.println("Had " + count + " commits overall in repository");
 
-                logs = git.log()
-                        // for all log.all()
-                        .addPath("README.md")
-                        .call();
-                count = 0;
-                for (RevCommit rev : logs) {
-                    //System.out.println("Commit: " + rev /* + ", name: " + rev.getName() + ", id: " + rev.getId().getName() */);
-                    count++;
-                }
-                System.out.println("Had " + count + " commits on README.md");
-
-                logs = git.log()
-                        // for all log.all()
-                        .addPath("pom.xml")
-                        .call();
-                count = 0;
-                for (RevCommit rev : logs) {
-                    //System.out.println("Commit: " + rev /* + ", name: " + rev.getName() + ", id: " + rev.getId().getName() */);
-                    count++;
-                }
-                System.out.println("Had " + count + " commits on pom.xml");
-
-                // then produce a "classic" Git commit log
-                // see also https://stackoverflow.com/a/69138290/411846
                 System.out.println();
-                System.out.println("Print log of commits on testbranch");
-                //logs = git.log()
-                      //  .add(repository.resolve("remotes/origin/testbranch"))
-                     //   .call();
-                //for (RevCommit rev : logs) {
-                  //  System.out.println(getConventionalCommitMessage(rev));
+                System.out.println("---------------------");
+                System.out.println("Local Commit Graph");
+
                 }
             }
         }
     }
+
+
 
    /*
    TODO: Replace deprecated modules on getConventialCommitMessage;
